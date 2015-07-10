ff719737bddd0b547c12195069602bf8334fcdd1
Jelmer de Maat <post@jelmerdemaat.nl>
2015-07-10 23:39:32 +0200

    default gulp setup

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	uglify = require('gulp-uglify'),
+	concat = require('gulp-concat'),
+	rename = require('gulp-rename');
+
+var src = 'src/',
+	dest = 'dist/';
+
+var html = {
+	src: src + '*.html',
+	dest: dest
+}
+
+var scss = {
+	src: src + 'sass/**/*.scss',
+	dest: dest + 'css/'
+}
+
+var javascript = {
+	src: src + 'javascript/**/*.js',
+	dest: dest + 'js/'
+}
+
+gulp.task('html', function() {
+	gulp.src(html.src)
+		.pipe(gulp.dest(html.dest));
+});
+
+gulp.task('scss', function() {
+  gulp.src(scss.src)
+    .pipe(sass({
+      outputStyle: 'compact'
+    }))
+    .pipe(gulp.dest(scss.dest));
+});
+
+gulp.task('javascript', function() {
+  gulp.src(javascript.src)
+    .pipe(uglify())
+    .pipe(gulp.dest(javascript.dest));
+});
+
+
+gulp.task('develop', [
+	'html',
+	'scss',
+	'javascript'
+]);
+
+gulp.task('default', ['develop'], function() {
+	gulp.watch(html.src, ['html']);
+	gulp.watch(scss.src, ['scss']);
+	gulp.watch(javascript.src, ['javascript']);
+});
